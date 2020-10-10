a7539be6f09403f02a4008a31642ccee81eaae21
Tokageki <58886109+tokageki@users.noreply.github.com>
2020-10-10 19:33:59 +0200

    Update pendu.py

--- pendu.py
+++ pendu.py
@@ -73,8 +73,8 @@ def dessinPendu(nb): #array qui contient les images
 
 with open("liste_de_mots.txt") as f: #ouvre la liste de mots
     lines = f.readlines() 
-    motATrouver = random.choice(lines)[:-1] #prends une ligne aleatoire dans le mot 
-
+    motATrouver = random.choice(lines)[:-1].upper() #prends une ligne aleatoire dans le mot 
+print(motATrouver)
 
 motJeu = ""
 nombreTentative = 0
@@ -109,11 +109,12 @@ while jeuEnCours:
             print("lettre déjà utilisée")
             pasPerdreVie = True #si c'est le cas, désactive la perte de vie dans ce tour
             lettreDansLaListe = True #si la lettre est déjà dans la liste "listeLettre", ce bool empèche d'ajouter la lettre dans la liste
-    if lettreDansLaListe == False : #si la lettre n'est pas déjà dans la liste, ça la met dedans
+    if lettreDansLaListe == False and pasPerdreVie == False: #si la lettre n'est pas déjà dans la liste, ça la met dedans
         listeLettres += lettre
 
     for i in range(len(motATrouver)):
         if lettre == motATrouver[i]: #verifie si la lettre est dans le mot
+            
             pasPerdreVie = True #si la lettre est dans le mot la variable est mise sur true pour dire de pas faire perdre de vie
             motJeu = motJeu[:i] + motATrouver[i] + motJeu[i+1:] #remplace le "-" correspondant à la lettre qui a été trouvée
     if pasPerdreVie == False : #fait perdre une vie
@@ -121,6 +122,7 @@ while jeuEnCours:
         print(dessinPendu(nombreTentative))
 
     if motJeu == motATrouver :
+        print(motATrouver)
         print("BRAVO ! TU AS GAGNE !") #je crois que c'est assez explicite
         break
         
