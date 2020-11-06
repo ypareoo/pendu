49f05d208e16f0e3571a316b44da50185e78ad12
Tokageki <58886109+tokageki@users.noreply.github.com>
2020-11-06 15:27:14 +0100

    Update penduWIPInterface.py

--- penduWIPInterface.py
+++ penduWIPInterface.py
@@ -1,3 +1,27 @@
+#Augeix Adrien
+
+# code source et tout les fichiers disponibles sur mon github
+# https://github.com/tokageki/pendu
+
+
+"""
+
+  888888                        8888888b.                8888888b.                        888
+    "88b                        888  "Y88b               888   Y88b                       888
+     888                        888    888               888    888                       888
+     888  .d88b.  888  888      888    888 888  888      888   d88P .d88b.  88888b.   .d88888 888  888
+     888 d8P  Y8b 888  888      888    888 888  888      8888888P" d8P  Y8b 888 "88b d88" 888 888  888
+     888 88888888 888  888      888    888 888  888      888       88888888 888  888 888  888 888  888
+     88P Y8b.     Y88b 888      888  .d88P Y88b 888      888       Y8b.     888  888 Y88b 888 Y88b 888
+     888  "Y8888   "Y88888      8888888P"   "Y88888      888        "Y8888  888  888  "Y88888  "Y88888
+   .d88P
+ .d88P"
+888P"
+
+"""
+
+
+import pygame
 import random #pour sortir un nombre au pif
 import tkinter #logique
 from tkinter import messagebox #pour plus tard
@@ -5,7 +29,7 @@ from tkinter import messagebox #pour plus tard
 
 root = tkinter.Tk() #initialise tkinter
 root.geometry("640x480") #fait une fenètre de 640x480 pixels
-root.resizable(False, False) #bloque la possiblilité de la redimensionner 
+root.resizable(False, False) #bloque la possiblilité de la redimensionner
 root.title('Jeu du pendu - AUGEIX Adrien') #mets le titre
 root.iconbitmap('game.ico') #met la MAGNIFIQUE icone faite par mes soins
 
@@ -28,35 +52,74 @@ def motTiret():#fait le mot avec les -
         motJeu += "-"
 
 
-motJeu = ""
-nombreTentative = 0
-jeuEnCours = True
-listeLettres = ""
-lettreDansLaListe = False
-rejouer = True
-pasPerdreVie = False
+def clearScreen(): #efface tout les elements de l'écran
+    global message, text, boutonValider, labelImage, texteJeu, messageError, boutonValider, boutonQuitter
+    message.destroy()
+    labelImage.destroy()
+    texteJeu.destroy()
+    messageError.destroy()
+    text.destroy()
+    boutonValider.destroy()
+    boutonRelancer.destroy()
+    boutonQuitter.destroy()
 
+    genGame() #relance une nouvelle partie
 
 
 def game(): #fonction qui vérifie si le joueur gagne ou pas et affiche l'avancement du jeu
-    global motJeu, motATrouver, jeuEnCours, nombreTentative, listeLettres, lettreDansLaListe, rejouer, partieCreee
-    if jeuEnCours:
-        texteJeu.config(text=motJeu)
-        lettreDansLaListe = False
-        pasPerdreVie = False #permet de perdre une vie dans ce tour
-        if nombreTentative >= 6 : #si le joueur n'a plus de vie il  a perdu
-            texteJeu.config(text="Tu as perdu\n le mot était "+ motATrouver)
-            jeuEnCours = False #coupe le jeu
-        elif motJeu == motATrouver :
-            texteJeu.config(text="Tu as gagné ! \n le mot était "+ motATrouver)
-            jeuEnCours = False
-            photo.config(file="cake.gif")
+    global motJeu, motATrouver, nombreTentative, listeLettres, lettreDansLaListe, partieCreee, boutonQuitter
+    texteJeu.config(text=motJeu)
+    lettreDansLaListe = False
+    pasPerdreVie = False #permet de perdre une vie dans ce tour
+    if nombreTentative >= 6 : #si le joueur n'a plus de vie il  a perdu
+        texteJeu.config(text="Tu as perdu\n le mot était "+ motATrouver)
+        text.destroy() #supprime les elements non nécéssaires et affiche les boutons pour relancer et quitter
+        boutonValider.destroy()
+        boutonRelancer.pack()
+        boutonQuitter.pack()
+    elif motJeu == motATrouver :
+        texteJeu.config(text="Tu as gagné ! \n le mot était "+ motATrouver)
+        photo.config(file="cake.gif") #affiche le gateau de portal quand on a gagné (hehe)
+        text.destroy() #supprime les elements non nécéssaires et affiche les boutons pour relancer et quitter
+        boutonValider.destroy()
+        boutonRelancer.pack()
+        boutonQuitter.pack()
 
+def quitter(): #ferme la fenètre
+    global root
+    root.destroy()
 
 def genGame(): #génère une nouvelle partie
-    genMot()
-    motTiret()
-    game()
+    global motJeu, motATrouver, nombreTentative, listeLettres, lettreDansLaListe, partieCreee, text, message, boutonValider, texteJeu, messageError,photo,labelImage, boutonRelancer, boutonQuitter
+    motJeu = "" #remets les valeurs à zero et les initialise si elles existent pas
+    nombreTentative = 0
+    listeLettres = "" #besoin de faire une variable vide pour faire des += plus tard
+    lettreDansLaListe = False
+    pasPerdreVie = False
+
+    text = tkinter.Entry(width = 10) #affiche les elements
+    message = tkinter.Label(text="Bienvenue dans le jeu du pendu")
+    boutonValider = tkinter.Button(root, text = "valider lettre", command = validerLettre, height=2, width=90)
+    texteJeu = tkinter.Label(text="")
+    messageError = tkinter.Label(text="")
+    photo = tkinter.PhotoImage(file="frame_0.gif")
+    labelImage = tkinter.Label(image=photo)
+    boutonRelancer = tkinter.Button(root, text = "Relancer une partie", command = clearScreen, height=2, width=90)
+    boutonQuitter = tkinter.Button(root, text = "Quitter", command = quitter, height=2, width=90)
+    labelImage.image = photo
+
+    message.pack() #pack tout
+    text.pack()
+    boutonValider.pack()
+    labelImage.pack()
+    texteJeu.pack()
+    messageError.pack()
+    boutonValider.pack()
+
+
+    genMot() #prends un mot dans la liste
+    motTiret() #transforme le mot en tirets
+    game() #lance le jeu
 
 
 
@@ -73,7 +136,7 @@ def validerLettre(): #fait fonctionner le bouton valider et lance le jeu
             messageError.config(text="") #efface le message d'erreur
     else :
         motJeu = motATrouver
-        pasPerdreVie = True  
+        pasPerdreVie = True
     for i in range(len(listeLettres)):
         if lettre == listeLettres[i]: #verifie si la lettre a déjà été utilisée
             messageError.config(text="lettre déjà utilisée")
@@ -89,28 +152,10 @@ def validerLettre(): #fait fonctionner le bouton valider et lance le jeu
     if pasPerdreVie == False : #fait perdre une vie
         nombreTentative += 1
         messageError.config(text="vous perdez une vie")
-        photo.config(file="frame_" + str(nombreTentative) + ".gif")
-    game()
+        photo.config(file="frame_" + str(nombreTentative) + ".gif") #affiche l'image du pendu
+    game() #continue de faire tourner le jeu, si on utilise des boucles l'interface freeze, noice
 
 
-text = tkinter.Entry(width = 10)
-message = tkinter.Label(text="Bienvenue dans le jeu du pendu")
-boutonValider = tkinter.Button(root, text = "valider lettre", command = validerLettre, height=2, width=90)
-texteJeu = tkinter.Label(text="")
-messageError = tkinter.Label(text="")
-photo = tkinter.PhotoImage(file="frame_0.gif")
-labelImage = tkinter.Label(image=photo)
-labelImage.image = photo 
-
-
-message.pack()
-text.pack()
-boutonValider.pack()
-labelImage.pack()
-texteJeu.pack()
-messageError.pack()
-genGame()
-
-
+genGame() #lance une partie quand le programme est ouvert
 
 root.mainloop()
